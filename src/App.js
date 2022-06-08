22d05788395df0fc3657c46c00d05c35c1c003f1
eric <ericwang42@outlook.com>
2022-06-07 17:40:06 -0700

    update accordion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript library",
+  },
+  {
+    title: "Why use React?",
+    content: "React is cool",
+  },
+  {
+    title: "is React used in Web3?",
+    content: "Yes",
+  },
+];
+
 function App() {
   return (
-    <div className="App">
-      <Accordion />
+    <div>
+      <Accordion items={items} />
     </div>
   );
 }
